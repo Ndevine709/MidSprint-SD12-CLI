d02afbe7fece94d27e3abe2435dfcb220bb20c05
ChrisM709 <christopher.meadus@keyin.com>
2025-06-20 10:20:34 -0300

    setup RESTClientTest based on example

--- Airport-CLI/src/test/java/com/airport/http/client/RESTClientTest.java
+++ Airport-CLI/src/test/java/com/airport/http/client/RESTClientTest.java
@@ -19,20 +19,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RESTClientTest {
 
+    // reate a MockWebServer which will act as a fake http server
     private static MockWebServer mockServer;
+
+    // RESTClient under test, which will send its requests to our mock server
     private RESTClient client;
 
+    // This method runs once before any @Test methods. It starts the MockWebServer and points our RESTClient at it.
     @BeforeAll
     static void startMockServer() throws Exception {
         mockServer = new MockWebServer();
         mockServer.start();
     }
 
+    // shuts down the MockWebServer so no ports remain in use
     @AfterAll
     static void shutdownMockServer() throws Exception {
         mockServer.shutdown();
     }
 
+    // ensures that for every test, we re-create a fresh RESTClient
     @BeforeEach
     void setUp() {
         String baseUrl = mockServer.url("/").toString();
